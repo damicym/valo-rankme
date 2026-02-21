ab09f8ef3fe5ec86a6faf4d8e36d8fa001d6627f
Axel Cymerman <axelcy@gmail.com>
2026-02-20 23:08:50 -0300

    api fixes for render service

--- api/app.js
+++ api/app.js
@@ -1,10 +1,11 @@
 import express from "express"
 import morgan from "morgan"
 import cors from "cors"
+import { config } from "./config"
 
 const app = express()
 
-app.set('port', process.env.PORT || 3000)
+app.set('port', config.PORT)
 
 // Desactivar caché durante desarrollo
 app.use((req, res, next) => {
--- api/config.js
+++ api/config.js
@@ -1,5 +1,6 @@
 import 'dotenv/config'
 
 export const config = {
-    HENRIKDEV_ACCESS_TOKEN: process.env.HENRIKDEV_ACCESS_TOKEN
+    HENRIKDEV_ACCESS_TOKEN: process.env.HENRIKDEV_ACCESS_TOKEN,
+    PORT: process.env.PORT || 3000
 }
