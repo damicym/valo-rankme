d85a8b37419cf3303d58ecfd2b36f599064326d4
Dami <damiancym05@gmail.com>
2026-03-31 03:43:33 -0300

    creo diccionario de eval params

--- api/src/valorant_config.js
+++ api/src/valorant_config.js
@@ -1,7 +1,70 @@
 export const IMMORTAL_ELO = 2100
+
 export const EVAL_PARAMS = {
-    min_rr: 7,
-    max_rr: 33,
-    min_acs: 80.0,
-    max_acs: 260.0
+    skirmish_2v2: {
+        min_rr: 7,
+        max_rr: 33,
+        min_acs: 80.0,
+        max_acs: 260.0
+    },
+    competitive: {
+        min_rr: 7,
+        max_rr: 33,
+        min_acs: 80.0,
+        max_acs: 260.0
+    },
+    escalation: {
+        min_rr: 7,
+        max_rr: 33,
+        min_acs: 80.0,
+        max_acs: 260.0
+    },
+    teamdeathmatch: {
+        min_rr: 7,
+        max_rr: 33,
+        min_acs: 80.0,
+        max_acs: 260.0
+    },
+    newmap: {
+        min_rr: 7,
+        max_rr: 33,
+        min_acs: 80.0,
+        max_acs: 260.0
+    },
+    replication: {
+        min_rr: 7,
+        max_rr: 33,
+        min_acs: 80.0,
+        max_acs: 260.0
+    },
+    snowballfight: {
+        min_rr: 7,
+        max_rr: 33,
+        min_acs: 80.0,
+        max_acs: 260.0
+    },
+    spikerush: {
+        min_rr: 7,
+        max_rr: 33,
+        min_acs: 80.0,
+        max_acs: 260.0
+    },
+    swiftplay: {
+        min_rr: 7,
+        max_rr: 33,
+        min_acs: 80.0,
+        max_acs: 260.0
+    },
+    unrated: {
+        min_rr: 7,
+        max_rr: 33,
+        min_acs: 80.0,
+        max_acs: 260.0
+    },
+    deathmatch: {
+        min_rr: 7,
+        max_rr: 33,
+        min_acs: 80.0,
+        max_acs: 260.0
+    }
 }
